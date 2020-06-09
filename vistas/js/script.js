f031c5295044ee89827ca781a37514c1dfbbc02b
Mario Alberto Rivera Aguilar <mario.rivera09@icloud.com>
2020-06-09 00:50:31 -0500

    Script de js

--- vistas/js/script.js
+++ vistas/js/script.js
@@ -7,45 +7,44 @@ document.getElementById("temp_lugar_hechos_cp").addEventListener("keyup", functi
       dataType: "json",
       url: 'vistas/ViewUbicacion.php?codigo_postal=' + $(this).val(),
       success: function (data) {
-        $('#temp_lugar_hechos_estado').html('');
-        for (var key in data) {
-          $('#temp_lugar_hechos_estado').html('<option value="' + data[key].codigo_estado + '">' + data[key].nombre_estado + '</option>');
 
-          $.ajax({
-            type: 'GET',
-            dataType: "json",
-            url: 'vistas/ViewUbicacion.php?codigo_cp=' + data[key].codigo_cp,
-            success: function (data2) {
-              $('#temp_lugar_hechos_colonia').html('');
-              for (var key2 in data2) {
-                $('#temp_lugar_hechos_colonia').append('<option value="' + data2[key2].codigo_colonia + '">' + data2[key2].nombre_colonia + '</option>');
+        if(data.length != 0){
+          $('#temp_lugar_hechos_estado').html('');
+          for (var key in data) {
+            $('#temp_lugar_hechos_estado').html('<option value="' + data[key].codigo_estado + '">' + data[key].nombre_estado + '</option>');
 
-                $.ajax({
-                  type: 'GET',
-                  dataType: "json",
-                  url: 'vistas/ViewUbicacion.php?codigo_municipio=' + data2[key2].codigo_municipio,
-                  success: function (data3) {
-                    $('#temp_lugar_hechos_municipio').html('');
-                    for (var key3 in data3) {
-                      $('#temp_lugar_hechos_municipio').html('<option value="' + data3[key3].codigo_municipio + '">' + data3[key3].nombre_municipio + '</option>');
+            $.ajax({
+              type: 'GET',
+              dataType: "json",
+              url: 'vistas/ViewUbicacion.php?codigo_cp=' + data[key].codigo_cp,
+              success: function (data2) {
+                $('#temp_lugar_hechos_colonia').html('');
+                for (var key2 in data2) {
+                  $('#temp_lugar_hechos_colonia').append('<option value="' + data2[key2].codigo_colonia + '">' + data2[key2].nombre_colonia + '</option>');
+
+                  $.ajax({
+                    type: 'GET',
+                    dataType: "json",
+                    url: 'vistas/ViewUbicacion.php?codigo_municipio=' + data2[key2].codigo_municipio,
+                    success: function (data3) {
+                      $('#temp_lugar_hechos_municipio').html('');
+                      for (var key3 in data3) {
+                        $('#temp_lugar_hechos_municipio').html('<option value="' + data3[key3].codigo_municipio + '">' + data3[key3].nombre_municipio + '</option>');
+                      }
                     }
-                  }
-                });
-
-                $('#temp_lugar_hechos_municipio').prop('disabled', false);
-
+                  });
+                }
               }
-            }
-          });
+            });
 
-          $('#temp_lugar_hechos_colonia').prop('disabled', false);
+            $('#temp_lugar_hechos_estado').prop('disabled', false);
+            $('#temp_lugar_hechos_colonia').prop('disabled', false);
+            $('#temp_lugar_hechos_municipio').prop('disabled', false);
 
+          }
         }
       }
     });
-
-    $('#temp_lugar_hechos_estado').prop('disabled', false);
-
   } else {
 
     $('#temp_lugar_hechos_estado').html('<option value="" disabled selected>Selecciona una opcion</option>');
